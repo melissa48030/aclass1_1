8765bbc0f4b2500ea50407d766f84f7f49fa88ad
melissa48030 <39421080+melissa48030@users.noreply.github.com>
2018-06-24 03:09:34 -0400

    a1

---
+++ aclass1_1/class1/assignments/a_02_29/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace a_02_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
